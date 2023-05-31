8c16eb9cdd04265dae7ee8aa6a66d05e7bcdd6a6
Saveily344 <guryanov.saveliy2014@yandex.ru>
2023-05-31 13:01:37 +0300

    Tic-tac-toe fixes

--- Tic-tac-toe/src/main/java/com/example/tictactoe/Game.java
+++ Tic-tac-toe/src/main/java/com/example/tictactoe/Game.java
@@ -3,47 +3,68 @@ package com.example.tictactoe;
 public class Game {
     //Счётчик ходов
     int counter;
-    //Очередь крестика
+    //Очередь крестика (синего)
     boolean turn = true;
     String winner = null;
     boolean isEnded = false;
     Field field;
+
     public Game(int size) {
         field = new Field(size);
     }
-    public void checkEnd() {
-        //Если не осталось свободных мест
-        if (counter == Math.pow(field.getSize(), 2)) isEnded = true;
+
+    public void checkEnd(int row, int col) {
+        //Если игра уже завершилась
         if (isEnded) return;
-        for (int i = 0; i < field.getSize(); i++){
-            //Проверка горизонтали
-            if (field.getCell(i, 0) != CellType.Nullity) {
-                for (int j = 1; j < field.getSize(); j++) {
-                    // Если текущая клетка горизонтали не равна предыдущей
-                    if (!field.getCell(i, j).equals(field.getCell(i, j - 1))) {
-                        break;
-                    }
-                    if (j == field.getSize() - 1){
-                        winner = field.getCell(i, 0).getCode();
-                        isEnded = true;
-                        return;
-                    }
+        //Если в данной клетке ничего не установлено
+        if (field.getCell(row, col).getCode().equals(CellType.Nullity.getCode())) return;
+        //Проверяем строку, соответствующую данной клетке
+        for (int i = 0; i < field.getSize() - 1; i++) {
+            //Если какой-то элемент данной строки не равен следующему, то проверка завершается
+            if (!field.getCell(row, i).getCode().equals(field.getCell(row, i + 1).getCode())) break;
+            //Если дошли до финальной клетки (все проверки прошли), то игра заканчивается
+            if (i == field.getSize() - 2) {
+                winner = field.getCell(row, col).getCode();
+                isEnded = true;
+                return;
+            }
+        }
+        //Проверяем столбец, соответствующий данной клетке
+        for (int i = 0; i < field.getSize() - 1; i++) {
+            //Если какой-то элемент данного столбца не равен следующему, то проверка завершается
+            if (!field.getCell(i, col).getCode().equals(field.getCell(i + 1, col).getCode())) break;
+            //Все проверки прошли
+            if (i == field.getSize() - 2) {
+                winner = field.getCell(row, col).getCode();
+                isEnded = true;
+                return;
+            }
+        }
+        //Если элемент расположен на левой диагонали (левая верхняя клетка лежит на ней)
+        if (row == col){
+            for (int i = 0; i < field.getSize() - 1; i++) {
+                //Если какой-то элемент левой диагонали не равен следующему, то проверка завершается
+                if (!field.getCell(i, i).getCode().equals(field.getCell(i + 1, i + 1).getCode())) break;
+                if (i == field.getSize() - 2) {
+                    winner = field.getCell(row, col).getCode();
+                    isEnded = true;
                 }
             }
-            //Проверка по вертикали
-            if (field.getCell(0, i) != CellType.Nullity) {
-                for (int j = 1; j < field.getSize(); j++) {
-                    // Если текущая клетка вертикали не равна предыдущей
-                    if (!field.getCell(j, i).equals(field.getCell(j - 1, i))) {
-                        break;
-                    }
-                    if (j == field.getSize() - 1){
-                        winner = field.getCell(0, i).getCode();
-                        isEnded = true;
-                        return;
-                    }
+        }
+        //Если клетка лежит на другой диагонали
+        if (row == field.getSize() - col - 1){
+            for (int i = 0; i < field.getSize() - 1; i++) {
+                //Если какой-то элемент другой диагонали не равен следующему, то проверка завершается
+                if (!field.getCell(i, field.getSize() - i - 1).getCode().equals(field.getCell(i + 1, field.getSize() - i - 2).getCode())) break;
+                if (i == field.getSize() - 2) {
+                    winner = field.getCell(row, col).getCode();
+                    isEnded = true;
                 }
             }
+        }
+        //Если не осталось свободных мест
+        if (counter == Math.pow(field.getSize(), 2)) {
+            isEnded = true;
         }
     }
 
@@ -51,8 +72,9 @@ public class Game {
     //передаётся аргумент, означающий тип клетки и координаты клетки
     public String makeStep(int x, int y) {
         if (isEnded) return null;
+        //Очередь сменяется
         CellType type = turn ? CellType.Blue : CellType.Red;
-        if (field.setCell(type, x, y)){
+        if (field.setCell(type, x, y)) {
             counter++;
             turn = !turn;
             return type.getCode();
@@ -65,10 +87,11 @@ class Field {
     //Размер игрового поля
     private final int size;
     private final CellType[][] field;
+
     /* Каждая ячейка поля имеет 3 значения:
      крестик, пустота, нолик
     */
-    public int getSize(){
+    public int getSize() {
         return size;
     }
 
@@ -93,13 +116,13 @@ class Field {
         }
     }
 
-    public CellType getCell(int i, int j){
+    public CellType getCell(int i, int j) {
         return field[i][j];
     }
 }
 
 enum CellType {
-    Red("red"), Blue("blue"), Nullity("");
+    Red("Red"), Blue("Blue"), Nullity("");
     private final String code;
 
     CellType(String code) {
